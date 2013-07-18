5587cb08a0469ca068467244cb2841014c6312ce
sharif-ghazzawi <sghazzawi@bericotechnologies.com>
2013-07-17 21:28:28 -0400

    trying to move menu button and make menu collapsable

--- js/controllers.js
+++ js/controllers.js
@@ -4,11 +4,14 @@
 
 angular.module('myApp.controllers', []).
   controller('MyCtrl1', ['$scope','$http',function($scope, $http) {
-  	$http({method: 'GET', url: '/Inhabitant/0'}).
-  	success(function(data, status, headers, config) {
-  		$scope.inhabitant = data;
-  	});
+	$http({method: 'GET', url: '/Inhabitant/0'}).
+	success(function(data, status, headers, config) {
+		$scope.inhabitant = data;
+	});
 
   }])
   .controller('MyCtrl2', [function() {
+  }])
+  .controller('CollapseDemoCtrl', ['$scope',function($scope){
+	$scope.isCollapsed = true;
   }]);
